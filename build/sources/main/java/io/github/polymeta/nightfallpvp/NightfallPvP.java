9a84cc9efab4c5d553067ce5fc83fc40c05f415b
Polymeta <40173118+Polymeta@users.noreply.github.com>
2021-01-07 03:27:15 +0000

    uploaded plugin

---
+++ build/sources/main/java/io/github/polymeta/nightfallpvp/NightfallPvP.java
@@ -0,0 +1,115 @@
+package io.github.polymeta.nightfallpvp;
+
+import com.google.inject.Inject;
+import com.griefdefender.api.GriefDefender;
+import com.griefdefender.api.Tristate;
+import com.griefdefender.api.claim.Claim;
+import com.griefdefender.api.permission.Context;
+import com.griefdefender.api.permission.flag.Flags;
+import com.griefdefender.api.permission.option.Options;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.world.World;
+import scala.actors.threadpool.Arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Plugin(id = "nightfallpvp",
+        name = "Nightfall PvP",
+        description = "Let PvP rule the nights, while days remain save.",
+        version = "1.0.4",
+        authors = {"Polymeta"})
+public class NightfallPvP
+{
+    @Inject
+    private Logger logger;
+
+    private boolean isDaylight = true;
+    private boolean isNightfall = true;
+
+    @Listener
+    public void onInit(GameInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event)
+    {
+        logger.info("Nightfall PvP by Polymeta!");
+        logger.info("locked and loaded");
+    }
+
+    @SubscribeEvent
+    public void onServerTick(TickEvent.ServerTickEvent event)
+    {
+        /*World world = FMLCommonHandler.instance().getMinecraftServerInstance().worlds[0];
+        if(world.isDaytime())
+        {
+            //it is daytime in world
+            if(isNightfall)
+            {
+                //it was night before; change
+                GriefDefender.getCore().getClaimManager(((org.spongepowered.api.world.World)world).getUniqueId()).getWildernessClaim().setOption(Options.PVP, "false", Collections.singleton(new Context("plugin", "nightfallpvp")));
+                isDaylight = true;
+                isNightfall = false;
+                logger.info("The Day rose, PvP has been deactivated");
+            }
+        }
+        else{
+            //it is nighttime
+            if(isDaylight)
+            {
+                //it was day before; change
+                GriefDefender.getCore().getClaimManager(((org.spongepowered.api.world.World)world).getUniqueId()).getWildernessClaim().setOption(Options.PVP, "true", Collections.singleton(new Context("plugin", "nightfallpvp")));
+                isDaylight = false;
+                isNightfall = true;
+                logger.info("The night came over, PvP has been activated");
+            }
+        }*/
+        //logger.info("tick");
+        Sponge.getServer().getWorldProperties(Sponge.getServer().getDefaultWorldName()).ifPresent(worldProperties -> {
+            int worldTime = (int) (worldProperties.getWorldTime() % 24000L);
+            //logger.warn(Integer.toString(worldTime));
+            if(worldTime > 13000 && worldTime < 23000)
+            {
+                //official nighttime
+                if(isDaylight)
+                {
+                    //it was day before; change
+                    Claim claim = GriefDefender.getCore().getClaimManager(worldProperties.getUniqueId()).getWildernessClaim();
+                    claim.setFlagPermission(Flags.ENTITY_DAMAGE, Tristate.TRUE, new HashSet<Context>(){{add(new Context("gd_claim", claim.getUniqueId().toString()));}});
+                    claim.setOption(Options.PVP, "true", new HashSet<Context>(){{add(new Context("gd_claim", claim.getUniqueId().toString()));}});
+                    isDaylight = false;
+                    isNightfall = true;
+                    logger.info("The night came over, PvP has been activated");
+                }
+            }
+            else{
+                //daytime dawn dusk etc
+                //it is daytime in world
+                if(isNightfall)
+                {
+                    //it was night before; change
+                    Claim claim = GriefDefender.getCore().getClaimManager(worldProperties.getUniqueId()).getWildernessClaim();
+                    claim.setFlagPermission(Flags.ENTITY_DAMAGE, Tristate.FALSE, new HashSet<Context>(){{add(new Context("gd_claim", claim.getUniqueId().toString()));}});
+                    claim.setOption(Options.PVP, "false", new HashSet<Context>(){{add(new Context("gd_claim", claim.getUniqueId().toString()));}});
+                    isDaylight = true;
+                    isNightfall = false;
+                    logger.info("The Day rose, PvP has been deactivated");
+                }
+            }
+        });
+    }
+}
